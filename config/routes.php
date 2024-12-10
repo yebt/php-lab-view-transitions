2cdd62b4777fd1f03f6133896250246c389f0468
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-12-09 19:24:44 -0500

    feat: add route file

--- config/routes.php
+++ config/routes.php
@@ -1,14 +1,14 @@
 <?php
 declare(strict_types=1);
 
-use App\Http\Router; 
+use App\Http\Router;
 
 $router = new Router();
 
 // Routes
 
-$router->add('/',  __DIR__ . '/../views/products/index.php');
-$router->add('/',  __DIR__ . '/../views/products/index.php');
+$router->add('/', __DIR__ . '/../views/products/index.php');
+$router->add('/', __DIR__ . '/../views/products/index.php');
 
 // Dispatch the router
 $router->dispatch();
